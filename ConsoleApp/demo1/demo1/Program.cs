3969ba3d7b0f609c6b32b55a53335cfd5695c34f
antlerjely96 <antlerjely96@gmail.com>
2023-05-09 08:41:56 +0700

    add demo class và các tính chất của OOP

---
+++ ConsoleApp/demo1/demo1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhập số nguyên a. Kiểm tra a là số nguyên âm hay dương
+
+//Khai báo a
+int a;
+//Nhập a
+Console.Write("a = ");
+a = int.Parse(Console.ReadLine());
+//Kiểm tra a
+if (a < 0)
+{
+    Console.WriteLine(a + " la so am");
+}
+else if (a > 0)
+{
+    Console.WriteLine(a + " la so duong");
+}
+else
+{
+    Console.WriteLine(a + " la so khong am khong duong");
+}
---
+++ ConsoleApp/demo2/demo2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Nhâp mảng có n phần tử nguyên. Tính tổng các phần tử của mảng
+ */
+ 
+ //Khai báo n
+ int n, tong = 0;
+ //Nhập n
+ Console.Write("n = ");
+ n = int.Parse(Console.ReadLine());
+ //Khai báo mảng
+ int[] arr = new int[n];
+//Nhập các phần tử của mảng
+ for (int i = 0; i < n; i++)
+ {
+  Console.Write("arr[" + i + "] = ");
+  arr[i] = int.Parse(Console.ReadLine());
+ }
+ //Tính tổng các phần tử của mảng
+ for (int i = 0; i < n; i++)
+ {
+  tong += arr[i];
+ }
+ //In ra tổng các phần tử của mảng
+ Console.WriteLine("Tong cac phan tu cua mang la: " + tong);
