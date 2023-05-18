e338753eb7f5f1b5f7530d5eaf5769fda5834107
antlerjely96 <antlerjely96@gmail.com>
2023-05-18 09:53:46 +0700

    demo CUD

--- WinformApp/demoCRUDCategory/demoCRUDCategory/Categories/CategoryList.Designer.cs
+++ WinformApp/demoCRUDCategory/demoCRUDCategory/Categories/CategoryList.Designer.cs
@@ -33,6 +33,15 @@ namespace demoCRUDCategory.Categories
         {
             this.label1 = new System.Windows.Forms.Label();
             this.ListCategory = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_cat_name = new System.Windows.Forms.TextBox();
+            this.tb_cat_id = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_add_new_cat = new System.Windows.Forms.Button();
+            this.btn_save_new_cat = new System.Windows.Forms.Button();
+            this.btn_edit_cat = new System.Windows.Forms.Button();
+            this.btn_update_cat = new System.Windows.Forms.Button();
+            this.btn_delete_cat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ListCategory)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +51,7 @@ namespace demoCRUDCategory.Categories
             this.label1.Cursor = System.Windows.Forms.Cursors.SizeAll;
             this.label1.Location = new System.Drawing.Point(183, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(526, 38);
+            this.label1.Size = new System.Drawing.Size(738, 329);
             this.label1.TabIndex = 0;
             this.label1.Text = "List Category";
             // 
@@ -50,22 +59,115 @@ namespace demoCRUDCategory.Categories
             // 
             this.ListCategory.AllowUserToAddRows = false;
             this.ListCategory.AllowUserToDeleteRows = false;
-            this.ListCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-            this.ListCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListCategory.BackgroundColor = System.Drawing.SystemColors.Window;
             this.ListCategory.GridColor = System.Drawing.SystemColors.Window;
             this.ListCategory.Location = new System.Drawing.Point(171, 81);
             this.ListCategory.Name = "ListCategory";
             this.ListCategory.ReadOnly = true;
             this.ListCategory.RowTemplate.Height = 40;
             this.ListCategory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.ListCategory.Size = new System.Drawing.Size(538, 322);
+            this.ListCategory.Size = new System.Drawing.Size(750, 613);
             this.ListCategory.TabIndex = 1;
+            this.ListCategory.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListCategory_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(170, 814);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category\'s name";
+            // 
+            // tb_cat_name
+            // 
+            this.tb_cat_name.Enabled = false;
+            this.tb_cat_name.Location = new System.Drawing.Point(453, 811);
+            this.tb_cat_name.Name = "tb_cat_name";
+            this.tb_cat_name.Size = new System.Drawing.Size(291, 38);
+            this.tb_cat_name.TabIndex = 3;
+            // 
+            // tb_cat_id
+            // 
+            this.tb_cat_id.Enabled = false;
+            this.tb_cat_id.Location = new System.Drawing.Point(453, 730);
+            this.tb_cat_id.Name = "tb_cat_id";
+            this.tb_cat_id.Size = new System.Drawing.Size(291, 38);
+            this.tb_cat_id.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(170, 726);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 42);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "category\'s id";
+            // 
+            // btn_add_new_cat
+            // 
+            this.btn_add_new_cat.Location = new System.Drawing.Point(784, 716);
+            this.btn_add_new_cat.Name = "btn_add_new_cat";
+            this.btn_add_new_cat.Size = new System.Drawing.Size(152, 64);
+            this.btn_add_new_cat.TabIndex = 6;
+            this.btn_add_new_cat.Text = "Add new";
+            this.btn_add_new_cat.UseVisualStyleBackColor = true;
+            this.btn_add_new_cat.Click += new System.EventHandler(this.btn_add_new_cat_Click);
+            // 
+            // btn_save_new_cat
+            // 
+            this.btn_save_new_cat.Enabled = false;
+            this.btn_save_new_cat.Location = new System.Drawing.Point(784, 797);
+            this.btn_save_new_cat.Name = "btn_save_new_cat";
+            this.btn_save_new_cat.Size = new System.Drawing.Size(152, 64);
+            this.btn_save_new_cat.TabIndex = 7;
+            this.btn_save_new_cat.Text = "Save";
+            this.btn_save_new_cat.UseVisualStyleBackColor = true;
+            this.btn_save_new_cat.Click += new System.EventHandler(this.btn_save_new_cat_Click);
+            // 
+            // btn_edit_cat
+            // 
+            this.btn_edit_cat.Location = new System.Drawing.Point(963, 716);
+            this.btn_edit_cat.Name = "btn_edit_cat";
+            this.btn_edit_cat.Size = new System.Drawing.Size(152, 64);
+            this.btn_edit_cat.TabIndex = 8;
+            this.btn_edit_cat.Text = "Edit";
+            this.btn_edit_cat.UseVisualStyleBackColor = true;
+            this.btn_edit_cat.Click += new System.EventHandler(this.btn_edit_cat_Click);
+            // 
+            // btn_update_cat
+            // 
+            this.btn_update_cat.Enabled = false;
+            this.btn_update_cat.Location = new System.Drawing.Point(963, 797);
+            this.btn_update_cat.Name = "btn_update_cat";
+            this.btn_update_cat.Size = new System.Drawing.Size(152, 64);
+            this.btn_update_cat.TabIndex = 9;
+            this.btn_update_cat.Text = "Update";
+            this.btn_update_cat.UseVisualStyleBackColor = true;
+            this.btn_update_cat.Click += new System.EventHandler(this.btn_update_cat_Click);
+            // 
+            // btn_delete_cat
+            // 
+            this.btn_delete_cat.Location = new System.Drawing.Point(1141, 716);
+            this.btn_delete_cat.Name = "btn_delete_cat";
+            this.btn_delete_cat.Size = new System.Drawing.Size(152, 64);
+            this.btn_delete_cat.TabIndex = 10;
+            this.btn_delete_cat.Text = "Delete";
+            this.btn_delete_cat.UseVisualStyleBackColor = true;
+            this.btn_delete_cat.Click += new System.EventHandler(this.btn_delete_cat_Click);
             // 
             // CategoryList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1120, 626);
+            this.ClientSize = new System.Drawing.Size(1332, 917);
+            this.Controls.Add(this.btn_delete_cat);
+            this.Controls.Add(this.btn_update_cat);
+            this.Controls.Add(this.btn_edit_cat);
+            this.Controls.Add(this.btn_save_new_cat);
+            this.Controls.Add(this.btn_add_new_cat);
+            this.Controls.Add(this.tb_cat_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_cat_name);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.ListCategory);
             this.Controls.Add(this.label1);
             this.Name = "CategoryList";
@@ -73,7 +175,21 @@ namespace demoCRUDCategory.Categories
             this.Load += new System.EventHandler(this.CategoryList_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ListCategory)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button btn_delete_cat;
+
+        private System.Windows.Forms.Button btn_edit_cat;
+        private System.Windows.Forms.Button btn_update_cat;
+
+        private System.Windows.Forms.TextBox tb_cat_id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_add_new_cat;
+        private System.Windows.Forms.Button btn_save_new_cat;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_cat_name;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView ListCategory;
--- WinformApp/demoCRUDCategory/demoCRUDCategory/Categories/CategoryList.cs
+++ WinformApp/demoCRUDCategory/demoCRUDCategory/Categories/CategoryList.cs
@@ -28,5 +28,93 @@ namespace demoCRUDCategory.Categories
             //Đổ Dataset vào DataGridView (phần hiển thị dữ liệu)
             ListCategory.DataSource = dataSet.Tables["categories"].DefaultView;
         }
+
+        private void btn_add_new_cat_Click(object sender, EventArgs e)
+        {
+            //Cho ô input nhập tên và nút save có thể tác động
+            tb_cat_name.Enabled = true;
+            btn_save_new_cat.Enabled = true;
+        }
+
+        private void btn_save_new_cat_Click(object sender, EventArgs e)
+        {
+            //Lấy dữ liệu từ ô nhập tên
+            string category_name = tb_cat_name.Text;
+            //Kết nối đến MySQL
+            string connectionString = "server=localhost;user id=root;password=;database=d01k13csharp";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            //Mở kết nối
+            connection.Open();
+            //Viết query
+            string sql = "INSERT INTO categories(name) VALUES (" + "'" + category_name + "'" + ")";
+            //Tạo command
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            //Chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            connection.Close();
+            //Load lại datagridview (load lại danh sách)
+            CategoryList_Load(null, null);
+        }
+
+        private void ListCategory_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Lấy id, name của row đang được chọn
+            string category_id = ListCategory.Rows[e.RowIndex].Cells["id"].Value.ToString();
+            string category_name = ListCategory.Rows[e.RowIndex].Cells["name"].Value.ToString();
+            //Hiển thị id, name của category vào ô input
+            tb_cat_id.Text = category_id;
+            tb_cat_name.Text = category_name;
+        }
+
+        private void btn_edit_cat_Click(object sender, EventArgs e)
+        {
+            //Cho ô input nhập tên và nút update có thể tác động
+            tb_cat_name.Enabled = true;
+            btn_update_cat.Enabled = true;
+        }
+
+        private void btn_update_cat_Click(object sender, EventArgs e)
+        {
+            //Lấy dữ liệu trong ô input
+            string cat_id = tb_cat_id.Text;
+            string cat_name = tb_cat_name.Text;
+            //Kết nối đến MySQL
+            string connectionString = "server=localhost;user id=root;password=;database=d01k13csharp";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            //Mở kết nối
+            connection.Open();
+            //Viết query
+            string sql = "UPDATE categories SET name = '" + cat_name + "' WHERE id = '" + cat_id + "'";
+            //Tạo đối tượng command
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            //Chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            connection.Close();
+            //Load lại danh sách
+            CategoryList_Load(null, null);
+        }
+
+        private void btn_delete_cat_Click(object sender, EventArgs e)
+        {
+            //Lấy id của bản ghi cần xóa từ ô input
+            string cat_id = tb_cat_id.Text;
+            //Kết nối đến MySQL
+            string connectionString = "server=localhost;user id=root;password=;database=d01k13csharp";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            //Mở kết nối
+            connection.Open();
+            //Viết query
+            string sql = "DELETE FROM categories WHERE id = '" + cat_id + "'";
+            //Tạo đối tượng command
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            //Chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            connection.Close();
+            //Load lại danh sách
+            CategoryList_Load(null, null);
+        }
     }
 }
